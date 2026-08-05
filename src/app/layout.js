da1446498cd0295fc5043d9cfceb7bcc77c93ddc
kendikadimas <dimas.fazrulfalah@mhs.unsoed.ac.id>
2026-08-05 12:01:08 +0700

    Add Google Analytics and GTM tracking tags

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,13 +1,84 @@
 import './globals.css';
+import Script from 'next/script';
+
+const siteName = 'Aquarius Massage Home Spa';
+const siteDescription = 'Aquarius Massage Home Spa menyediakan pijat panggilan ke rumah, hotel, apartemen, dan lokasi Anda di Jakarta, Tangerang, Bekasi, Depok, BSD, dan Serpong. Pilih Traditional Massage, Relaxing Massage, Aromatherapy Massage, atau Reflexology & Body Spa dengan terapis wanita berpengalaman dan privasi terjaga.';
+const siteKeywords = [
+  'aquarius massage home spa',
+  'aquarius spa',
+  'pijat panggilan jakarta',
+  'home spa jakarta',
+  'pijat panggilan tangerang',
+  'pijat panggilan bekasi',
+  'pijat panggilan depok',
+  'pijat hotel',
+  'pijat apartemen',
+  'massage home spa',
+  'traditional massage',
+  'relaxing massage',
+  'aromatherapy massage',
+  'reflexology body spa',
+  'terapis wanita profesional',
+  'spa panggilan'
+];
+
+const structuredData = {
+  '@context': 'https://schema.org',
+  '@type': 'HealthAndBeautyBusiness',
+  name: siteName,
+  description: siteDescription,
+  telephone: '0813-1188-515',
+  areaServed: ['Jakarta', 'Tangerang', 'Bekasi', 'Depok', 'BSD', 'Serpong'],
+  openingHours: 'Mo-Su 10:00-00:00',
+  priceRange: 'Rp300.000 - Rp400.000',
+  serviceType: ['Traditional Massage', 'Relaxing Massage', 'Aromatherapy Massage', 'Reflexology & Body Spa'],
+  image: '/images/big-logo.png',
+};
 
 export const metadata = {
-  title: 'Aquarius Massage Home Spa | Layanan Pijat Panggilan Relaksasi',
-  description: 'Layanan pijat profesional panggilan ke lokasi Anda (Rumah, Hotel, Apartemen) di Jakarta, Tangerang, & Bekasi. Traditional Massage, Relaxing Massage, Aromatherapy Spa. Hubungi 0813-1188-515.',
-  keywords: ['aquarius spa', 'home spa jakarta', 'pijat panggilan', 'massage home spa', 'traditional massage', 'aromatherapy massage', 'pijat tgr beka'],
+  applicationName: siteName,
+  metadataBase: new URL(process.env.NEXT_PUBLIC_SITE_URL ?? 'https://aquariushomespa.com'),
+  title: {
+    default: 'Aquarius Massage Home Spa | Pijat Panggilan Jakarta, Tangerang & Bekasi',
+    template: `%s | ${siteName}`,
+  },
+  description: siteDescription,
+  keywords: siteKeywords,
+  robots: {
+    index: true,
+    follow: true,
+    googleBot: {
+      index: true,
+      follow: true,
+      'max-image-preview': 'large',
+      'max-snippet': -1,
+      'max-video-preview': -1,
+    },
+  },
+  icons: {
+    icon: '/icon.png',
+    apple: '/apple-icon.png',
+  },
   openGraph: {
-    title: 'Aquarius Massage Home Spa - Relax Your Body, Refresh Your Mind',
-    description: 'Pijat profesional panggilan langsung ke tempat Anda. Terapis wanita berpengalaman, 100% privasi terjamin.',
+    title: 'Aquarius Massage Home Spa | Pijat Panggilan Jakarta, Tangerang & Bekasi',
+    description: siteDescription,
     type: 'website',
+    locale: 'id_ID',
+    siteName,
+    images: [
+      {
+        url: '/images/big-logo.png',
+        width: 2000,
+        height: 2000,
+        alt: siteName,
+      },
+    ],
+  },
+  twitter: {
+    card: 'summary_large_image',
+    title: 'Aquarius Massage Home Spa | Pijat Panggilan Jakarta, Tangerang & Bekasi',
+    description: siteDescription,
+    images: ['/images/big-logo.png'],
   },
 };
 
@@ -15,7 +86,34 @@ export default function RootLayout({ children }) {
   return (
     <html lang="id">
       <body className="antialiased bg-[#0a0806] text-[#e6dfd5] selection:bg-[#d4af37] selection:text-stone-950">
+        <Script src="https://www.googletagmanager.com/gtag/js?id=AW-18367956526" strategy="afterInteractive" />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'AW-18367956526');
+          `}
+        </Script>
+        <Script id="google-tag-manager" strategy="afterInteractive">
+          {`
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-KV4VNTH4');
+          `}
+        </Script>
+        <noscript
+          dangerouslySetInnerHTML={{
+            __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-KV4VNTH4" height="0" width="0" style="display:none;visibility:hidden"></iframe>`
+          }}
+        />
         {children}
+        <script
+          type="application/ld+json"
+          dangerouslySetInnerHTML={{ __html: JSON.stringify(structuredData) }}
+        />
       </body>
     </html>
   );
